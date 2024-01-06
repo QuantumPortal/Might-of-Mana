224361c75f136690b791aa21c1a4684368f7cf65
Glenn Dossot <glenn@dossot.net>
2024-01-06 15:26:36 -0800

    Update UI, Continued modularization.

---
+++ src/client/camera.lua
@@ -0,0 +1,19 @@
+local cameraFunctions = {}
+
+local TweenService = game:GetService('TweenService')
+local cameraTween = TweenInfo.new(
+	0.375,
+	Enum.EasingStyle.Linear,
+	Enum.EasingDirection.In,
+	0,
+	false,
+	0
+)
+local previousCameraTilt = 0
+
+function cameraFunctions.CameraTilt(camera,cameraTilt)
+    TweenService:Create(camera,cameraTween, { CFrame = camera.CFrame * CFrame.Angles(0,0,math.rad(cameraTilt - previousCameraTilt))}):Play()
+    previousCameraTilt = cameraTilt
+end
+
+return cameraFunctions
--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,4 +1,3 @@
-
 local Players = game:GetService("Players")
 local player = game:GetService("Players").LocalPlayer
 
@@ -10,61 +9,56 @@ local mouse = player:GetMouse()
 local character = player.Character or player.CharacterAdded:Wait()
 local rootPart = character:FindFirstChild("HumanoidRootPart") or character:WaitForChild("HumanoidRootPart")
 local humanoid = character:FindFirstChild("Humanoid") or character:WaitForChild("Humanoid")
-local upperTorso = character:FindFirstChild("UpperTorso") or character:WaitForChild("UpperTorso")
-local waist = upperTorso:FindFirstChild("Waist") or upperTorso:WaitForChild("Waist")
-local head = character:FindFirstChild("Head") or character:WaitForChild("Head")
-local neck = head:FindFirstChild("Neck") or head:WaitForChild("Neck")
 local camera = workspace.Camera
 local cameraTilt = 0
 local baseWalkSpeed = 9
 
 local characterFunctions = require(script.character)
+local cameraFunctions = require(script.camera)
 --local fireball = game.ReplicatedStorage.Shared:FindFirstChild("Fireball") or game.ReplicatedStorage.Shared:WaitForChild("Fireball")
 
-local cameraTween = TweenInfo.new(
-	0.7,
-	Enum.EasingStyle.Linear,
-	Enum.EasingDirection.Out,
-	0,
-	false,
-	0
-)
-
-humanoid.WalkSpeed = baseWalkSpeed
-humanoid.AutoRotate = false
-
-game:GetService("UserInputService").InputBegan:Connect(function(input, _gameProcessed)
+UserInputService.InputBegan:Connect(function(input, _gameProcessed)
 	if input.KeyCode == Enum.KeyCode.E then
 		--fireball:FireServer(player);
 	elseif input.KeyCode == Enum.KeyCode.Q then
 		player:SetAttribute("Mana",player:GetAttribute("Mana") - 20)
 		player:SetAttribute("Shield",player:GetAttribute("Shield") - 10)
-	end
+	end	
 end)
 
+humanoid.WalkSpeed = baseWalkSpeed
+humanoid.AutoRotate = false
+
+player.CharacterAdded:Connect(function(character)
+	humanoid = character:WaitForChild("Humanoid")
+	humanoid.WalkSpeed = baseWalkSpeed
+	humanoid.AutoRotate = false
+end)
 
 RunService.RenderStepped:Connect(function(delay)
-	characterFunctions.DirectionalTilt(player)
-	characterFunctions.LookToMouse(player)
-	
-
-	--[[
 	if UserInputService:IsKeyDown(Enum.KeyCode.A) and UserInputService:IsKeyDown(Enum.KeyCode.D) then
-		cameraTilt *= 0.95
+		cameraTilt *= 0.93
+		cameraTilt = math.sign(cameraTilt) * math.floor(math.abs(cameraTilt)*100)/100
 	elseif UserInputService:IsKeyDown(Enum.KeyCode.A) then
-		cameraTilt += (14 - cameraTilt)/11
+		print("fwaa")
+		cameraTilt += (14-cameraTilt)/8
+		cameraTilt = math.ceil(cameraTilt*100)/100
 	elseif UserInputService:IsKeyDown(Enum.KeyCode.D) then
-		cameraTilt -= (14 + cameraTilt)/11
+		cameraTilt += (-14-cameraTilt)/8
+		cameraTilt = math.floor(cameraTilt*100)/100
 	else
-		cameraTilt *= 0.95
+		cameraTilt *= 0.93
+		cameraTilt = math.sign(cameraTilt) * math.floor(math.abs(cameraTilt)*100)/100
 	end
-	]]--
-
-
-
-	TweenService:Create(camera,cameraTween, { CFrame = camera.CFrame * CFrame.Angles(0,0,math.rad(cameraTilt))}):Play()
 	
 
+	print(cameraTilt)
+	if player.Character.Humanoid and player.Character.Humanoid:GetState() ~= Enum.HumanoidStateType.Dead then
+		characterFunctions.DirectionalTilt(player)
+		characterFunctions.LookToMouse(player)
+		cameraFunctions.CameraTilt(camera, cameraTilt)
+	end
+	
 	if UserInputService:IsKeyDown(Enum.KeyCode.LeftShift) then
 		if humanoid.WalkSpeed < baseWalkSpeed + 10 then
 			humanoid.WalkSpeed += (baseWalkSpeed + 10 - humanoid.WalkSpeed)/6
@@ -75,14 +69,12 @@ RunService.RenderStepped:Connect(function(delay)
 		end
 		
 	end 
-
-	
-
-
 	camera.FieldOfView = 70 - 9 + humanoid.WalkSpeed
 
-	
 end)
+
+
+
 
 --[[
 player.CharacterAdded:Connect(function(character)
